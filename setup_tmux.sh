9422ab880c68685e065389377df9266aa528df19
root <root@wuklab-05.sysnet.ucsd.edu>
2020-02-09 14:14:16 -0500

    add vbundle tpm download script

---
+++ setup_tmux.sh
@@ -0,0 +1,7 @@
+#
+# After you run this script, open a tmux seesion,
+# press [Prefix + I] (capital i, as in Install) to fetch the plugin
+#
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
