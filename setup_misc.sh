2132ac2efc177fe0165b636db1c4086ba962cc51
lastweek <syzwhat@gmail.com>
2020-09-04 16:48:08 -0700

    Chill.

--- setup_misc.sh
+++ setup_misc.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 
-##
+#
 # bpytop
 #
 # Resource Monitor
 # 1. https://github.com/aristocratos/bpytop
 # 2. Print CPU/Mem/Disk and various info in one place
 pip3 install bpytop --upgrade
+
+#
+# tig
+#
+# Cool GIT!
+# https://github.com/jonas/tig/blob/master/INSTALL.adoc
