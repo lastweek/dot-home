0d2e30e0296ff50f14f9ab9bd5963777635dfe0c
Yizhou Shan <syzwhat@gmail.com>
2020-12-15 16:07:49 -0800

    sd

---
+++ all.sh
@@ -0,0 +1,7 @@
+#
+# You have to manually run setup_zsh_2.sh after this one..
+#
+./setup_vim.sh
+./setup_neovim.sh
+./setup_tmux.sh
+./setup_zsh_1.sh
--- setup_vim.sh
+++ setup_vim.sh
@@ -2,5 +2,8 @@
 mv ~/.vimrc ~/.vimrc_backup
 ln -s $(pwd)/.vimrc ~/.vimrc
 
+mv ~/.vim ~/.vim_backup
+ln -s $(pwd)/.vim ~/.vim
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
