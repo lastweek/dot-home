24c1cdf9e31fb1031b4a6ebfa2d5f049a0b1798c
Yizhou Shan <syzwhat@gmail.com>
2020-12-21 11:15:59 -0800

    asd

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,6 +3,7 @@ mv ~/.vimrc ~/.vimrc_backup
 ln -s $(pwd)/.vimrc ~/.vimrc
 
 mv ~/.vim ~/.vim_backup
+mv ~/.vim ~/.vim_backup
 ln -s $(pwd)/.vim ~/.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
