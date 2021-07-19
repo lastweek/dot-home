16af9800a9e3a11edf1f9367aa64b0ccd780003a
Yizhou Shan <syzwhat@gmail.com>
2021-07-19 00:52:50 -0700

    sdasd

--- setup_zsh_2.sh
+++ setup_zsh_2.sh
@@ -4,7 +4,7 @@ ln -s $(pwd)/.zshrc ~/.zshrc
 
 # Copy themes
 mv ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme.backup
-ln -s $(pwd)/oh-my-zsh/themes/robbyrussell.zsh-theme
+ln -s $(pwd)/oh-my-zsh/themes/robbyrussell.zsh-theme  ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 
 # Clone plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
